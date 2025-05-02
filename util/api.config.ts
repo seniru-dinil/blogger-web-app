1efb01ab53bce41ca44e2bda1c8dd2bc377b1875
seniru-dinil <senirudinil927@gmail.com>
2025-05-02 20:01:35 +0530

    api configuration done

---
+++ util/api.config.ts
@@ -0,0 +1,20 @@
+export const API_BASE_URL = "http://localhost:4004";
+
+export const URL = {
+  ARTICLE: {
+    CREATE_ARTICLE: "/api/articles",
+    GET_ARTICLE_LIST: "/api/articles",
+    GET_ARTICLE: "/api/articles",
+    DELETE_ARTICLE: "/api/articles",
+    UPDATE_ARTICLE: "/api/articles",
+    LIKE_ARTICLE: "/api/articles/like",
+    COMMENT_ARTICLE: "/api/articles/comment",
+    GET_ARTICLE_LIST_BY_CATEGORY: "/api/articles/category",
+  },
+  USERS: {
+    CREATE_USER: "/api/users",
+    DELETE_USER: "/api/users",
+    LOGIN: "/api/users/login",
+    GET_USER: "/api/users",
+  },
+};
---
+++ util/api.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { API_BASE_URL } from "./api.config";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    console.log("Request:", {
+      url: config.url,
+      method: config.method,
+      headers: config.headers,
+      data: config.data,
+    });
+    return config;
+  },
+  (error) => {
+    console.log("request error ===> ", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
