054d2d414d0f5654ba91c3c32818b6e7fb419c44
seniru-dinil <senirudinil927@gmail.com>
2025-05-11 12:07:32 +0530

    feat: update API endpoints for article management and data handling

--- api/api.config.ts
+++ api/api.config.ts
@@ -3,13 +3,21 @@ export const API_BASE_URL = "http://localhost:4004";
 export const URL = {
   ARTICLE: {
     CREATE_ARTICLE: "/api/articles",
-    GET_ARTICLE_LIST: "/api/articles",
+    GET_ARTICLES: "/api/articles",
     GET_ARTICLE: "/api/articles",
     DELETE_ARTICLE: "/api/articles",
     UPDATE_ARTICLE: "/api/articles",
-    LIKE_ARTICLE: "/api/articles/like",
-    COMMENT_ARTICLE: "/api/articles/comment",
-    GET_ARTICLE_LIST_BY_CATEGORY: "/api/articles/category",
+    LIKE_ARTICLE: "/api/articles/",
+    COMMENT_ARTICLE: "/api/articles/",
+    PUBLISH_ARTICLE: "/api/articles/",
+    UNPUBLISH_ARTICLE: "/api/articles/",
+    REPORT_ARTICLE: "/api/articles/",
+  },
+  ARTICLE_DATA: {
+    CREATE_ARTICLE_DATA: "/api/article-data",
+    UPDATE_ARTICLE_DATA: "/api/article-data",
+    DELETE_ARTICLE_DATA: "/api/article-data",
+    GET_ARTICLE_DATA: "/api/article-data",
   },
   USERS: {
     CREATE_USER: "/api/users",
