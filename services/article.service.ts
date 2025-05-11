bdbcb9784ce58538814b9ffcb416d9ad5a45ae58
seniru-dinil <senirudinil927@gmail.com>
2025-05-11 13:40:54 +0530

    feat: enhance article service with CRUD operations and comment functionality

--- services/article.service.ts
+++ services/article.service.ts
@@ -1,5 +1,33 @@
 import api from "@/api/api";
 import { URL } from "@/api/api.config";
+import Article from "@/model/article.model";
+import Comment from "@/model/comment.model";
 
-export const createArticle = (data: any) =>
+export const createArticle = (data: Article) =>
   api.post(URL.ARTICLE.CREATE_ARTICLE, data);
+
+export const deleteArticle = (id: number) =>
+  api.delete(`${URL.ARTICLE.DELETE_ARTICLE}/${id}`);
+
+export const updateArticle = (id: number, data: Article) =>
+  api.patch(`${URL.ARTICLE.UPDATE_ARTICLE}/${id}`, data);
+
+export const getArticle = (id: number) =>
+  api.get(`${URL.ARTICLE.GET_ARTICLE}/${id}`);
+
+export const getArticleList = () => api.get(URL.ARTICLE.GET_ARTICLES);
+
+export const publishArticle = (id: number) =>
+  api.post(`${URL.ARTICLE.PUBLISH_ARTICLE}/${id}/publish`);
+
+export const unpublishArticle = (id: number) =>
+  api.post(`${URL.ARTICLE.UNPUBLISH_ARTICLE}/${id}/unpublish`);
+
+export const likeArticle = (id: number) =>
+  api.post(`${URL.ARTICLE.LIKE_ARTICLE}/${id}/like`);
+
+export const reportArticle = (id: number) =>
+  api.post(`${URL.ARTICLE.REPORT_ARTICLE}/${id}/report`);
+
+export const commentArticle = (id: number, comment: Comment) =>
+  api.post(`${URL.ARTICLE.COMMENT_ARTICLE}/${id}/comment`, comment);
