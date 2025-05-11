9343484ce3880b573cac09d9ecd3ee0f34b67a95
seniru-dinil <senirudinil927@gmail.com>
2025-05-11 17:32:50 +0530

    feat: update user roles in createUser function to include ROLE_PUBLISHER

--- services/user.service.ts
+++ services/user.service.ts
@@ -5,5 +5,5 @@ import User from "@/model/user.model";
 export const createUser = (user: User) =>
   api.post(URL.USERS.CREATE_USER, {
     ...user,
-    roles: ["ROLE_VISITOR"],
+    roles: ["ROLE_VISITOR", "ROLE_PUBLISHER"],
   });
