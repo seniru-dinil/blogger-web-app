b0b0b933cf4e972ab34dc145c5e07e71af6524a3
seniru-dinil <senirudinil927@gmail.com>
2025-05-15 12:00:25 +0530

    feat: implement CRUD operations for article data in articleData.service.ts

--- services/articleData.service.ts
+++ services/articleData.service.ts
@@ -4,3 +4,12 @@ import ArticleData from "@/model/articleData.model";
 
 export const createArticleData = (articleData: ArticleData) =>
   api.post(URL.ARTICLE_DATA.CREATE_ARTICLE_DATA, articleData);
+
+export const deleteArticleData = (id: number) =>
+  api.delete(`${URL.ARTICLE_DATA.DELETE_ARTICLE_DATA}/${id}`);
+
+export const getArticleData = (id: number) =>
+  api.get(`${URL.ARTICLE_DATA.GET_ARTICLE_DATA}/${id}`);
+
+export const updateArticleData = (id: number, articleData: ArticleData) =>
+  api.patch(`${URL.ARTICLE_DATA.UPDATE_ARTICLE_DATA}/${id}`, articleData);
