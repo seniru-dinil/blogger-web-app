62269168d108fbcd699dfcf70db92910ad61a429
seniru-dinil <senirudinil927@gmail.com>
2025-05-18 20:48:06 +0530

    removed header from home page

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="p-4">
-      <h1>hello world</h1>
+      <div className="flex md:flex-row flex-col">
+        <div className="flex-1"></div>
+      </div>
     </div>
   );
 }
